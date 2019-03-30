a4feb65643bf213e71bd4c121d46c534b750199b
kosugikun <kosukisaki@yahoo.co.jp>
2019-03-30 23:28:08 +0900

    ファイルリセット

--- musicbot/constants.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-MAIN_VERSION = '1.2.1'
-SUB_VERSION = '-リリース'
-VERSION = MAIN_VERSION + SUB_VERSION
-
-AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
-DISCORD_MSG_CHAR_LIMIT = 2000
