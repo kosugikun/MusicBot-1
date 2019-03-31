9b691fd04e0b24f6dc56dd3e932ed34a19ebac62
Kosugi_kun <33971692+kosugikun@users.noreply.github.com>
2019-03-31 16:56:06 +0900

    バージョン割り当て

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.3.0'
-SUB_VERSION = '-Beta2'
+SUB_VERSION = '-Beta3'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
